a3443e20a81898614a4c00d4c744afb97c613e51
Shung Fei <git@shungfei.com>
2025-07-23 15:45:07 +1200

    enter normal mode in terminal with esc
    need to launch lazygit with snacks instead of toggleterm to still use
    esc in lazygit

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -80,12 +80,17 @@ return {
           desc = "Close buffer from tabline",
         },
 
+        ["<Leader>gg"] = { function() require("snacks").lazygit() end, desc = "Lazygit" },
+
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
+      },
+      t = {
+        ["<esc>"] = { [[<C-\><C-n>]] },
       },
     },
   },
