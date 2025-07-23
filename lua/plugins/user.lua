bf51c27cc734dfc9f729ee3e1ce61a7a0a57b80d
Shung Fei <git@shungfei.com>
2025-07-23 15:42:41 +1200

    add extra niceties to dashboard

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -43,6 +43,8 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = {
+        width = 69,
+        pane_gap = 16,
         preset = {
           header = table.concat({
             [[                                                                     ]],
@@ -56,6 +58,77 @@ return {
             [[██████  █████████████████████ ████ █████ █████ ████ ██████]],
             [[                                                                     ]],
           }, "\n"),
+        },
+
+        -- https://github.com/folke/snacks.nvim/discussions/111#discussioncomment-11382576
+        sections = {
+          { section = "header" },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+          function()
+            local in_git = require("snacks").git.get_root() ~= nil
+            return {
+              pane = 2,
+              icon = " ",
+              desc = "Browse Repo",
+              enabled = in_git,
+              padding = 1,
+              key = "b",
+              action = function() require("snacks").gitbrowse() end,
+            }
+          end,
+          function()
+            local in_git = require("snacks").git.get_root() ~= nil
+            local cmds = {
+              -- GitHub stuff is slooooow: see https://github.com/folke/snacks.nvim/issues/1769
+              -- {
+              --   title = "Notifications",
+              --   cmd = "gh notify -s -a -n5",
+              --   action = function() vim.ui.open "https://github.com/notifications" end,
+              --   key = "n",
+              --   icon = " ",
+              --   height = 5,
+              --   enabled = true,
+              -- },
+              -- {
+              --   title = "Open Issues",
+              --   cmd = "gh issue list -L 3",
+              --   key = "i",
+              --   action = function() vim.fn.jobstart("gh issue list --web", { detach = true }) end,
+              --   icon = " ",
+              --   height = 7,
+              -- },
+              -- {
+              --   icon = " ",
+              --   title = "Open PRs",
+              --   cmd = "gh pr list -L 3",
+              --   key = "P",
+              --   action = function() vim.fn.jobstart("gh pr list --web", { detach = true }) end,
+              --   height = 7,
+              -- },
+              {
+                icon = " ",
+                title = "Git Status",
+                cmd = "git --no-pager diff --stat -B -M -C",
+                height = 10,
+              },
+            }
+            return vim.tbl_map(
+              function(cmd)
+                return vim.tbl_extend("force", {
+                  pane = 2,
+                  section = "terminal",
+                  enabled = in_git,
+                  padding = 1,
+                  ttl = 5 * 60,
+                  indent = 3,
+                }, cmd)
+              end,
+              cmds
+            )
+          end,
+          { section = "startup" },
         },
       },
     },
