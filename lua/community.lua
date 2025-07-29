270032ecc7cddca4b991d1315071d68f05edb229
Shung Fei <git@shungfei.com>
2025-07-29 22:51:18 +1200

    add tailwind pack from astrocommunity

--- lua/community.lua
+++ lua/community.lua
@@ -28,6 +28,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.yaml" },
 }
