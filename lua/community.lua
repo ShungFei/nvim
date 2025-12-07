70c2fd172c5a833ec1f8b1f6b9abb8e297669856
Shung Fei <git@shungfei.com>
2025-12-07 21:50:17 +1300

    stop using copilot

--- lua/community.lua
+++ lua/community.lua
@@ -6,8 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.completion.codeium-vim" },
-  { import = "astrocommunity.completion.copilot-vim" },
-  { import = "astrocommunity.editing-support.codecompanion-nvim" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.game.leetcode-nvim" },
   { import = "astrocommunity.markdown-and-latex.markview-nvim" },
